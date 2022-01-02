98c94923da58335ce18183a84ebd7c667f1e0466
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2022-01-02 20:22:34 +0700

    fix:

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(process.env.PORT || 3000);
 setInterval(function () {
   http.get("http://gaixinhbot.herokuapp.com");
   console.log("Wakeup Heroku-Chan !!");
-}, 300000); // every 5 minutes (300000)
+}, 3000); // every 5 minutes (300000)
 
 bot.onText(/\. (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
